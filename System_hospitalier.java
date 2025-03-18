e193f403317a4e409de72ee679eed0329a7a53de
Bruniaux <mathis.bruniaux@gmail.com>
2025-03-18 15:40:15 +0100

    Modification System_hospitalier

---
+++ System_hospitalier.java
@@ -0,0 +1,7 @@
+analyserRequete : Cette méthode est appelée lorsqu’une nouvelle requête est ajoutée
+via recevoirRequete() et permet de renvoie "requête analysée"
+
+comparerDonnees : Elle compare les données des requêtes stockées dans listRequetes et 
+renvoie "Cluster trouvé"
+
+
